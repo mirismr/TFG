9d1720bfa12c591763ec28089f720417a27fac06
Míriam Mengíbar Rodríguez <mirismr@correo.ugr.es>
2018-04-02 01:43:17 +0200

    funcion working probar get_number_images #17

--- Keras/count/imagenet_count.py
+++ Keras/count/imagenet_count.py
@@ -2,25 +2,48 @@
 import codecs
 import _pickle as cPickle
 
-classes = {}
-images = 0
+'''
+Hay que pasarle fichero .txt con las URL de todas las imagenes
+XXXX_YYYY -> XXXX: WNID, YYYY: imagen
+'''
+def count_images_per_class(file):
+	classes = {}
+	images = 0
 
-with codecs.open("fall11_urls.txt", "rb",encoding='utf-8', errors='ignore') as f:
-	for i, line in enumerate(f):
-		label, _ = line.split("\t", 1)
-		wnid, _ = label.split("_")
-		if wnid in classes:
-			classes[wnid] += 1
-		else:
-			classes[wnid] = 1
-		images += 1
+	with codecs.open(file, "rb", encoding='utf-8', errors='ignore') as f:
+		for i, line in enumerate(f):
+			label, _ = line.split("\t", 1)
+			wnid, _ = label.split("_")
+			if wnid in classes:
+				classes[wnid] += 1
+			else:
+				classes[wnid] = 1
+			images += 1
 
-# Output
-print("Classes: %i" % len(classes))
-print("Images: %i" % images)
 
-print(classes)
-cPickle.dump(classes, open("count_classes", "wb"))
+	print("Classes: %i" % len(classes))
+	print("Images: %i" % images)
+
+	return classes
+
+'''Devuelve el nºimagenes de un nodo del arbol (incluido el y sus hijos)'''
+def get_number_images(nodo):
+	total = classes[nodo.name]
+
+	children = nodo.children
+	for c in children:
+		if not c.is_leaf:
+			total = total + get_number_images(c)
+
+	return total
+
+
+#classes = count_images_per_class("fall11_urls.txt")
+#cPickle.dump(classes, open("count_classes", "wb"))
+#probar sin funciona
+classes = cPickle.load(open("count_classes", "rb"))
+raiz = cPickle.load(open("../tree_sysnet_python/tree_n12992868", "rb"))
+print(get_number_images(raiz))
 
 '''
 class_counts = [count for _, count in classes.items()]
